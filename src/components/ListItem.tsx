48c5fcbf647fd9e882c64a6c50b742a89de9bab0
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-07-14 15:55:39 +0100

    styled buttons

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,13 +1,21 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { Button, Paper, Typography } from "@mui/material";
 import { ContentData } from "../types";
 
 export default function ListItem({ content }: { content: ContentData }) {
 	return (
 		<Paper elevation={2} className="p-1">
-			<div className="flex flex-row items-center">
-				<Typography className="grow">{content.name}</Typography>
-				<Button>~</Button>
-				<Button>-</Button>
+			<div className="flex flex-row items-center gap-2">
+				<Typography variant="body1" className="grow">
+					{content.name}
+				</Typography>
+				<Button variant="contained" color="secondary" className="!min-w-8 !p-0 aspect-square ">
+					<EditIcon />
+				</Button>
+				<Button variant="contained" color="error" className="!min-w-8 !p-0 aspect-square ">
+					<DeleteIcon />
+				</Button>
 			</div>
 		</Paper>
 	);
