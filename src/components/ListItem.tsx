e93736af69dd8d655817650f396bc70295bb9f00
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-07-14 16:08:09 +0100

    moved edit button to be by the title

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,18 +1,17 @@
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
-import { Button, Paper, Typography } from "@mui/material";
+import { Button, IconButton, Paper, Typography } from "@mui/material";
 import { ContentData } from "../types";
 
 export default function ListItem({ content }: { content: ContentData }) {
 	return (
 		<Paper elevation={6} className="p-1">
 			<div className="flex flex-row items-center gap-2">
-				<Typography variant="body1" className="grow">
-					{content.name}
-				</Typography>
-				<Button variant="contained" color="primary" className="!min-w-8 !p-0 aspect-square ">
-					<EditIcon />
-				</Button>
+				<Typography variant="body1">{content.name}</Typography>
+				<IconButton size="small" className="self-end !text-[12px] !-ml-2">
+					<EditIcon fontSize="inherit" />
+				</IconButton>
+				<span className="grow" />
 				<Button variant="contained" color="error" className="!min-w-8 !p-0 aspect-square ">
 					<DeleteOutlineIcon />
 				</Button>
