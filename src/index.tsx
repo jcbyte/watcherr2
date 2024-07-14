18d9d7f3a593c380a1636b41dbecff6d5d5383c2
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-07-14 15:04:31 +0100

    dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,24 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
