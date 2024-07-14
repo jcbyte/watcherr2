13faba15d6b48e6f7c8438859b8e46b70b0f8e9d
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-07-14 15:36:34 +0100

    added buttons to each content item

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,10 +1,14 @@
-import { Paper } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 import { ContentData } from "../types";
 
 export default function ListItem({ content }: { content: ContentData }) {
 	return (
-		<div>
-			<Paper elevation={2}>{content.name}</Paper>
-		</div>
+		<Paper elevation={2} className="p-1">
+			<div className="flex flex-row items-center">
+				<Typography className="grow">{content.name}</Typography>
+				<Button>~</Button>
+				<Button>-</Button>
+			</div>
+		</Paper>
 	);
 }
