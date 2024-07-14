2785e1090c346f64a20162ae39d5ee5f44faf7c8
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-07-14 16:01:16 +0100

    updated theme

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,20 +1,20 @@
-import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
 import { Button, Paper, Typography } from "@mui/material";
 import { ContentData } from "../types";
 
 export default function ListItem({ content }: { content: ContentData }) {
 	return (
-		<Paper elevation={2} className="p-1">
+		<Paper elevation={6} className="p-1">
 			<div className="flex flex-row items-center gap-2">
 				<Typography variant="body1" className="grow">
 					{content.name}
 				</Typography>
-				<Button variant="contained" color="secondary" className="!min-w-8 !p-0 aspect-square ">
+				<Button variant="contained" color="primary" className="!min-w-8 !p-0 aspect-square ">
 					<EditIcon />
 				</Button>
 				<Button variant="contained" color="error" className="!min-w-8 !p-0 aspect-square ">
-					<DeleteIcon />
+					<DeleteOutlineIcon />
 				</Button>
 			</div>
 		</Paper>
--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,42 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const darkTheme = createTheme({
+const theme = createTheme({
 	palette: {
 		mode: "dark",
+		primary: {
+			main: "#5e35b1",
+		},
+		secondary: {
+			main: "#3949ab",
+		},
+		error: {
+			main: "#e53935",
+		},
+		warning: {
+			main: "#fb8c00",
+		},
+		info: {
+			main: "#757575",
+		},
+		success: {
+			main: "#43a047",
+		},
+		background: {
+			default: "#1e1e1e",
+		},
+		text: {
+			primary: "#dcdcdc",
+			secondary: "rgba(220,220,220,0.7)",
+			disabled: "rgba(220,220,220,0.5)",
+		},
 	},
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={darkTheme}>
+		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<App />
 		</ThemeProvider>
